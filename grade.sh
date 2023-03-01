8dcfd96c66ff09030a43fed13318bfa61588b112
Kat-thy-W <115119572+Kat-thy-W@users.noreply.github.com>
2023-03-01 11:11:20 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -3,3 +3,32 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+
+cd student-submission
+
+if [[ -f ListExamples.java ]]
+then
+        echo "ListExamples.java found"
+else
+        echo "ListExamples.java is missing"
+        exit 1
+fi
+
+cd ..
+
+cp student-submission/ListExamples.java ./
+set +e
+javac -cp $CPATH *.java 2>compile.txt
+
+if [[ $? -eq 0 ]]
+then
+        echo "Files compiled"
+else
+        echo "Compilation error"
+        cat compile.txt
+        exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 2>results.txt
+cat results.txt
